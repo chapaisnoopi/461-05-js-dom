9787934c73cc5641ba6c9acf7189ed64e32795da
chapaisnoopi <57961752+chapaisnoopi@users.noreply.github.com>
2020-02-19 17:13:34 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -46,4 +46,13 @@ function clear() {
 }
 
 //$('.some-fun').click(() => $('p').toggle(3000));
-$('.some-fun').click(() => $('p').blur());
+$('.some-fun').click(function() {
+  $( "p" ).first().animate({
+    left: 100
+  }, {
+    duration: 1000,
+    step: function( now, fx ){
+      $( "p" ).slice( 1 ).css( "left", now );
+    }
+  });
+});
